aad227c123e6dde98588e076e1475203a4db8060
Raúl R. Pearson <raulrpearson@protonmail.com>
2019-03-13 19:48:24 +0700

    Add WIP package.json parcel etc.

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
-console.log('This is working MOFO!');
+// const auth = require('solid-auth-client');
+// import upperCase from 'lodash.uppercase';
+// var startCase = require('lodash.startcase');
+// const auth = require('solid-auth-client');
+import auth from './solid-auth-client.bundle.js';
 
 // Check out https://stackoverflow.com/a/46388898
 // We want the pageAction to always be shown/active
@@ -11,6 +15,12 @@ browser.pageAction.onClicked.addListener(tab => {
     type: 'basic',
     iconUrl: browser.extension.getURL('icons/icon.svg'),
     title: 'Bookmarked!',
+    // message: `${tab.url} ${upperCase(tab.url)} ${startCase(tab.url)}`
     message: tab.url
+  });
+
+  auth.trackSession(session => {
+    if (!session) console.log('The user is not logged in');
+    else console.log(`The user is ${session.webId}`);
   });
 });
